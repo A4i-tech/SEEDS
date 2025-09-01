a803af3608bc060ccb0dc1c530cded71367db101
Farman <farmanahmed888@gmail.com>
2025-09-01 15:26:56 +0530

    Refactor verifyToken to support loading Firebase service account from environment variable or file

--- backend-server/auth/verifyToken.js
+++ backend-server/auth/verifyToken.js
@@ -1,7 +1,8 @@
 "use strict";
 const admin = require("firebase-admin");
 const path = require("path");
- 
+const fs = require("fs");
+
 /*
 create a separate serviceAccountKey.json file for your own Firebase Account
 To generate private key follow below steps,
@@ -12,29 +13,41 @@ To generate private key follow below steps,
     5. Under Firebase admin SDK section, you should be able to see 'Generate new private key' option
     6. when you click on this option, it will prompt you to download a json file that you can place exactly in the place of this serviceAccountKey.json file
 */
- 
-const serviceAccount = path.join(__dirname, "serviceAccountKey.json")
- 
+
+let serviceAccount;
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    // Load from environment variable
+    try {
+        serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+    } catch (e) {
+        console.error("Invalid FIREBASE_SERVICE_ACCOUNT env variable: not valid JSON");
+        throw e;
+    }
+} else {
+    // Load from file (local/dev)
+    serviceAccount = require(path.join(__dirname, "serviceAccountKey.json"));
+}
+
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 })
- 
+
 async function verifyToken(req, res, next) {
     try{
         console.log(`\nRequest:\n`
             + `body: ${JSON.stringify(req.body)}\n`
             + `query: ${JSON.stringify(req.query)}\n`
             + `params: ${JSON.stringify(req.params)}`)
-    
+
         const authToken = req.headers['authtoken']
         console.log(`authtoken: ${authToken}`)
         if(authToken == "postman" || authToken == "postman1") {
             req.userId = `postman@gmail.com`
             return next()
         }
-    
+
         //regex for a phone number
-    
+
         else if(authToken.startsWith("+91") && authToken.length == 13){
             req.userId = authToken
             return next()
@@ -42,9 +55,9 @@ async function verifyToken(req, res, next) {
     }catch(error){
         res.sendStatus(401);
     }
- 
+
     // try {
-    //     const token = await admin.auth().verifyIdToken(authToken)
+    //     const token = await admin.auth().verifyIdToken(authToksen)
     //     console.log(JSON.stringify(token))
     //     req.userId = token.phone_number
     //     console.log(`userId: ${req.userId}`)
@@ -54,5 +67,5 @@ async function verifyToken(req, res, next) {
     //     res.sendStatus(401);
     // }
 }
- 
+
 module.exports = verifyToken
