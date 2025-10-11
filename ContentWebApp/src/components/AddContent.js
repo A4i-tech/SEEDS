58978acf47b44a22289787dc7a8d4adc9f601b50
Farman Ahmed <farmanahmed888@gmail.com>
2025-10-11 17:13:20 +0530

    Enhance AddQuiz tests with MemoryRouter and improve test coverage; update setupTests to suppress React Router warnings

--- ContentWebApp/src/components/AddContent.js
+++ ContentWebApp/src/components/AddContent.js
@@ -7,38 +7,36 @@ const AddContent = () => {
   const [experience, setExperience] = useState("Story");
 
   const location = useLocation();
-  console.log("link props", location.state);
 
   const handleChange = (event) => {
     setExperience(event.target.value);
-    console.log(event.target.value);
   };
 
   return (
     <>
-      <div style={{margin:"20px"}}>
+      <div style={{ margin: "20px" }}>
         <h3>Add Content</h3>
         <form>
           <label>
             Pick your experience:
-            <br/>
+            <br />
             <select
               value={experience}
               onChange={(event) => handleChange(event)}
               className="mintgreen"
-              style={{width:"150px"}}
+              style={{ width: "150px" }}
             >
               <option value="Story">Story</option>
               <option value="Poem">Poem</option>
               <option value="Song">Song</option>
               <option value="Snippet">Snippet</option>
               {/* <option value="Riddle">Riddle</option> */}
-              <option value="quiz">Quiz</option> 
+              <option value="quiz">Quiz</option>
             </select>
           </label>
         </form>
         {experience === "quiz" && <AddQuiz />}
-        {(experience !== "quiz") && <AddStory contentType={experience} />}
+        {experience !== "quiz" && <AddStory contentType={experience} />}
       </div>
     </>
   );
--- ContentWebApp/src/setupTests.js
+++ ContentWebApp/src/setupTests.js
@@ -2,4 +2,22 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+// Suppress React Router v7 future flag warnings in tests
+const originalWarn = console.warn;
+beforeAll(() => {
+  console.warn = (...args) => {
+    if (
+      typeof args[0] === "string" &&
+      args[0].includes("React Router Future Flag Warning")
+    ) {
+      return;
+    }
+    originalWarn.call(console, ...args);
+  };
+});
+
+afterAll(() => {
+  console.warn = originalWarn;
+});
