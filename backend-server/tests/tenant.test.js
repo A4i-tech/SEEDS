22158652eea5d0b8f8294413dca4a441c0f97a27
Punit Kaushik <punit.kaushik444@gmail.com>
2025-10-29 14:08:06 +0530

    Adding Azure Storage account name and key test evs in teneant.test.js

--- backend-server/tests/tenant.test.js
+++ backend-server/tests/tenant.test.js
@@ -1,5 +1,9 @@
 process.env.AUTH_TYPE = 'native';
 process.env.SECRET_KEY = 'test-secret-key-for-testing-purposes-123';
+
+process.env.AZURE_STORAGE_ACCOUNT_NAME = 'mockaccountname';
+process.env.AZURE_STORAGE_ACCOUNT_KEY = 'mockkeymockkeymockkeymockkeymockkeymockkeymockkeymockkey';
+
 const request = require('supertest');
 const app = require('../src/index');
 const mongoose = require('mongoose');
