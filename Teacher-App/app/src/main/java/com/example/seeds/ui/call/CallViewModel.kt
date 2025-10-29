52ab336ecd222e45c3e5295c37addda5621483aa
Punit Kaushik <punit.kaushik444@gmail.com>
2025-10-29 16:17:22 +0530

    Minor Fixes

--- Teacher-App/app/src/main/java/com/example/seeds/ui/call/CallViewModel.kt
+++ Teacher-App/app/src/main/java/com/example/seeds/ui/call/CallViewModel.kt
@@ -283,16 +283,6 @@ class CallViewModel @Inject constructor(
         }
     }
 
-    fun updateClassroomContent(classroom: Classroom) {
-        viewModelScope.launch {
-            try {
-                classroomRepository.updateClassroom(classroom)
-            } catch (e: Exception) {
-                Log.e("CallViewModel", "Failed to update classroom", e)
-            }
-        }
-    }
-
     fun setSelectedContent(content: Content){
         _selectedContent.value = content
     }
