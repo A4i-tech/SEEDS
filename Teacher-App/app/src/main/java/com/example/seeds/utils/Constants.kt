9a3674af2d879757ec931a08b2a1a9b63b090df8
Punit Kaushik <122111594+Pun-it@users.noreply.github.com>
2025-10-29 11:02:12 +0530

    Clear  Constants
    Removed hardcoded URLs from Constants class.

--- Teacher-App/app/src/main/java/com/example/seeds/utils/Constants.kt
+++ Teacher-App/app/src/main/java/com/example/seeds/utils/Constants.kt
@@ -2,9 +2,10 @@ package com.example.seeds.utils
 
 class Constants {
     companion object {
-        const val BASE_URL = "https://web-service-hefq.onrender.com"
-        const val CONTENT_URL = "https://conferencev2.onrender.com"
+        const val BASE_URL = ""
+        const val CONTENT_URL = ""
         const val APP_VERSION = "1.0.0"
     }
 }
+
 
