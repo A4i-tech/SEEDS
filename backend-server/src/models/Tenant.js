ebb971fe0e2b5a2c813f2c57ab9a2c61ba492341
Farman Ahmed <farmanahmed888@gmail.com>
2025-10-30 15:11:28 +0530

    Update Tenant schema: rename name to tenantName and add indexes to email and tenantName fields

--- backend-server/src/models/Tenant.js
+++ backend-server/src/models/Tenant.js
@@ -2,10 +2,10 @@
 
 const mongoose = require("mongoose");
 const tenantSchema = new (require("mongoose").Schema)({
-        email: {type: String, required: true, unique: true},
-        password: {type: String, required: true},
-        name: {type: String, required: true}
-    }, {timestamps: true}
+    email: {type: String, required: true, unique: true, index: true},
+    password: {type: String, required: true},
+    tenantName: {type: String, required: true, index: true}
+  }, {timestamps: true}
 );
 
 const Tenant = mongoose.model("Tenant", tenantSchema);
