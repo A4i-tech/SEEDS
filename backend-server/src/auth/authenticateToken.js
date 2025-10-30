59f84127826377277fbce411c423fc0b25f0c1f2
Farman Ahmed <farmanahmed888@gmail.com>
2025-10-30 15:45:08 +0530

    Refactor authenticateToken: remove Firebase support and simplify token verification logic

--- backend-server/src/auth/authenticateToken.js
+++ backend-server/src/auth/authenticateToken.js
@@ -6,44 +6,28 @@ const {STATUS} = require('../config/constants');
 
 // Ensure secretKey is defined for native auth
 if (authType === 'native' && (!secretKey || typeof secretKey !== 'string' || secretKey.trim() === '')) {
-    throw new Error('secretKey environment variable must be defined and non-empty for native authentication');
+  throw new Error('secretKey environment variable must be defined and non-empty for native authentication');
 }
 
 // Ensure Firebase is initialized for Firebase auth
 if (authType === 'firebase' && !admin.apps.length) {
-    let serviceAccount;
-    if (firebaseServiceAccount) {
-        serviceAccount = JSON.parse(firebaseServiceAccount);
-    } else {
-        serviceAccount = require('./serviceAccountKey.json');
-    }
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
-    });
+  let serviceAccount = JSON.parse(firebaseServiceAccount);
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
 }
 
 function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if (!token) return res.sendStatus(STATUS.UNAUTHORIZED);
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) return res.sendStatus(STATUS.UNAUTHORIZED);
 
-    if (authType === 'native') {
-        jwt.verify(token, secretKey, (err, user) => {
-            if (err) return res.sendStatus(STATUS.FORBIDDEN);
-            req.user = user;
-            req.userId = user.id ;
-            next();
-        });
-    } else if (authType === 'firebase') {
-        admin.auth().verifyIdToken(token)
-            .then((decodedToken) => {
-                req.user = decodedToken;
-                next();
-            })
-            .catch(() => res.sendStatus(STATUS.FORBIDDEN));
-    } else {
-        res.sendStatus(STATUS.UNAUTHORIZED);
-    }
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) return res.sendStatus(STATUS.FORBIDDEN);
+    req.user = user;
+    req.userId = user.id;
+    next();
+  });
 }
 
 module.exports = authenticateToken;
