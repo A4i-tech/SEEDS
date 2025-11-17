a8a053e4e8c2e3cc3eb19c0574954399f381e91e
Punit Kaushik <punit.kaushik444@gmail.com>
2025-11-17 13:23:12 +0530

    Encryption revamp

--- Teacher-App/app/src/main/java/com/example/seeds/utils/NativeEncryptor.kt
+++
@@ -1,7 +0,0 @@
-package com.example.seeds.utils
-
-object NativeEncryptor {
-    init { System.loadLibrary("native-lib") }
-    external fun encrypt(data: String, key: ByteArray, iv: ByteArray): ByteArray
-    external fun decrypt(encryptedData: ByteArray, key: ByteArray, iv: ByteArray): String
-}
