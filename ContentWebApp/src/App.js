5a3fb2e6daa48ab883fa7d88eb4653e8091f0a0f
Farman Ahmed <farmanahmed888@gmail.com>
2026-01-02 13:59:23 +0530

    Wrap routes in Suspense for lazy loading; improve loading experience in App component

--- ContentWebApp/src/App.js
+++ ContentWebApp/src/App.js
@@ -1,60 +1,65 @@
-import AllContent from "./components/AllContent";
+import { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ContentDetails from "./components/ContentDetails";
-import ContentEdit from "./components/ContentEdit";
-import AddContent from "./components/AddContent";
-import BulkCallInitiator from "./components/BulkCallInitiator";
-import IVR from "./components/IVR";
-import ViewIVR from "./components/ViewIVR";
-import Profile from "./components/Profile";
 import "./App.css";
-import Login from "./components/Login";
-import ApiDocumentation from "./components/ApiDocumentation";
-import Register from "./components/Register";
 import ProtectedRoute from "./components/ProtectedRoute";
 import PublicRoute from "./components/PublicRoute";
+
+const AllContent = lazy(() => import("./components/AllContent"));
+const ContentDetails = lazy(() => import("./components/ContentDetails"));
+const ContentEdit = lazy(() => import("./components/ContentEdit"));
+const AddContent = lazy(() => import("./components/AddContent"));
+const BulkCallInitiator = lazy(() => import("./components/BulkCallInitiator"));
+const IVR = lazy(() => import("./components/IVR"));
+const ViewIVR = lazy(() => import("./components/ViewIVR"));
+const Profile = lazy(() => import("./components/Profile"));
+const Login = lazy(() => import("./components/Login"));
+const Register = lazy(() => import("./components/Register"));
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<PublicRoute element={<Login />} />}></Route>
-          <Route
-            path="/content"
-            element={<ProtectedRoute element={<AllContent />} />}
-          />
-          <Route
-            path="/content/create"
-            element={<ProtectedRoute element={<AddContent />} />}
-          />
-          <Route
-            path="/content/detail/:type/:id"
-            element={<ProtectedRoute element={<ContentDetails />} />}
-          />
-          <Route
-            path="/content/edit/:type/:id"
-            element={<ProtectedRoute element={<ContentEdit />} />}
-          />
-          <Route path="/ivr" element={<ProtectedRoute element={<IVR />} />} />
-          <Route
-            path="/viewivr"
-            element={<ProtectedRoute element={<ViewIVR />} />}
-          />
-          <Route
-            path="/bulkcall"
-            element={<ProtectedRoute element={<BulkCallInitiator />} />}
-          />
-          <Route path="/api-docs" element={<ApiDocumentation />} />
-          <Route
-            path="/register"
-            element={<PublicRoute element={<Register />} />}
-          />
-          <Route
-            path="/profile"
-            element={<ProtectedRoute element={<Profile />} />}
-          />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route
+              path="/"
+              element={<PublicRoute element={<Login />} />}
+            ></Route>
+            <Route
+              path="/content"
+              element={<ProtectedRoute element={<AllContent />} />}
+            />
+            <Route
+              path="/content/create"
+              element={<ProtectedRoute element={<AddContent />} />}
+            />
+            <Route
+              path="/content/detail/:type/:id"
+              element={<ProtectedRoute element={<ContentDetails />} />}
+            />
+            <Route
+              path="/content/edit/:type/:id"
+              element={<ProtectedRoute element={<ContentEdit />} />}
+            />
+            <Route path="/ivr" element={<ProtectedRoute element={<IVR />} />} />
+            <Route
+              path="/viewivr"
+              element={<ProtectedRoute element={<ViewIVR />} />}
+            />
+            <Route
+              path="/bulkcall"
+              element={<ProtectedRoute element={<BulkCallInitiator />} />}
+            />
+            <Route
+              path="/register"
+              element={<PublicRoute element={<Register />} />}
+            />
+            <Route
+              path="/profile"
+              element={<ProtectedRoute element={<Profile />} />}
+            />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </div>
   );
