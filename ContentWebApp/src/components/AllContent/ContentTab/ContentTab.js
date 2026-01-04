13df6678459982fbf68ac2034827ec2f956f2378
Farman Ahmed <farmanahmed888@gmail.com>
2026-01-04 17:30:48 +0530

    Enhance content filtering: add reset functionality and manage selected filters (#105)
    Enhance content filtering functionality by adding selected values and multiselect reference; update related components to support new filter reset logic.

--- ContentWebApp/src/components/AllContent/ContentTab/ContentTab.js
+++ ContentWebApp/src/components/AllContent/ContentTab/ContentTab.js
@@ -14,6 +14,7 @@ const ContentTab = ({
   paginationInfo,
   isFiltered,
   options,
+  selectedValues,
   onFilterChange,
   onResetFilters,
   onUpdateIVR,
@@ -22,6 +23,7 @@ const ContentTab = ({
   onDelete,
   onLoadMore,
   isUpdatingIVR,
+  multiselectRef,
 }) => {
   const navigate = useNavigate();
 
@@ -53,7 +55,12 @@ const ContentTab = ({
         </div>
       </div>
 
-      <ContentFilters options={options} onFilterChange={onFilterChange} />
+      <ContentFilters
+        options={options}
+        selectedValues={selectedValues}
+        onFilterChange={onFilterChange}
+        multiselectRef={multiselectRef}
+      />
 
       <ContentTable
         content={content}
