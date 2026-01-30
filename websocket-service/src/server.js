8804697e40954245225189f67a5964fb7e6e9e63
theainerd <beingprofess@gmail.com>
2026-01-30 23:48:34 +0530

    feat(ml-audio): Implement streaming audio analysis and hold detection

--- websocket-service/src/server.js
+++ websocket-service/src/server.js
@@ -7,6 +7,7 @@ const appInsights = require("applicationinsights");
 const websocketService = require("./services/websocketService");
 const controlService = require("./services/controlService");
 const connectionManager = require("./services/connectionManager");
+const mlStreamService = require("./services/mlStreamService");
 
 const port = process.env.PORT || 3000;
 const MAXIMUM_CONFERENCE_TIME_ALLOWED_IN_MILLISECONDS = 60 * 60 * 1000; // 1 hour in milliseconds
